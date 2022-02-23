5c895a9aa5dfe136995df2833e3d2697130f2fe8
dereknahman <44810431+dereknahman@users.noreply.github.com>
2022-02-23 15:08:25 +0000

    remove sass config

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,16 @@ const { parallel, watch } = require('gulp');
 
 const images = require('./gulp-tasks/images.js');
 
-// Pull in each task
-const sass = require('./gulp-tasks/sass.js');
-
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
     watch('./src/images/**/*', { ignoreInitial: true }, images);
-    watch('./src/scss/**/*.scss', { ignoreInitial: true }, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(images, sass);
+exports.default = parallel(images);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
