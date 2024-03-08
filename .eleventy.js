e166ae0b9bd8179a66efacb706e82c6391581b91
Kirsty Simmonds <kirsty.simmonds@ft.com>
2024-03-08 16:51:37 +0000

    when will i fucking learn

--- .eleventy.js
+++ .eleventy.js
@@ -1,93 +1,25 @@
 const path = require("path");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const filters = require("./utils/filters.js");
-
+const sanitizeHTML = require("sanitize-html");
+const dateFilter = require("./src/filters/date-filter");
+const webmentionsFilter = require("./src/filters/webmentions");
 const Image = require("@11ty/eleventy-img");
 
 module.exports = (config) => {
     config.addPassthroughCopy("src/img");
 
-    config.addPlugin(pluginRss);
-
-    // Object.keys(filters).forEach((filterName) => {
-    //     config.addFilter(filterName, filters[filterName]);
-    // });
-    // WEBMENTIONS FILTER
-    config.addFilter("webmentionsForUrl", (webmentions, url) => {
-        // console.log("urgh", webmentions);
-
-        // define which types of webmentions should be included per URL.
-        // possible values listed here:
-        // https://github.com/aaronpk/webmention.io#find-links-of-a-specific-type-to-a-specific-page
-        const allowedTypes = [
-            "mention-of",
-            "in-reply-to",
-            "like-of",
-            "repost-of",
-            "bookmark-of",
-        ];
-
-        // define which HTML tags you want to allow in the webmention body content
-        // https://github.com/apostrophecms/sanitize-html#what-are-the-default-options
-        const allowedHTML = {
-            allowedTags: ["b", "i", "em", "strong", "a"],
-            allowedAttributes: {
-                a: ["href"],
-            },
-        };
-
-        // clean webmention content for output
-        const clean = (entry) => {
-            console.log("entry -> ", entry);
-            const { html, text } = entry.content;
-
-            if (html) {
-                // really long html mentions, usually newsletters or compilations
-                entry.content.value =
-                    html.length > 2000
-                        ? `mentioned this in <a href="${entry["wm-source"]}">${entry["wm-source"]}</a>`
-                        : sanitizeHTML(html, allowedHTML);
-            } else {
-                entry.content.value = sanitizeHTML(text, allowedHTML);
-            }
-
-            return entry;
-        };
-
-        // sort webmentions by published timestamp chronologically.
-        // swap a.published and b.published to reverse order.
-        const orderByDate = (a, b) =>
-            new Date(b.published) - new Date(a.published);
-
-        // only allow webmentions that have an author name and a timestamp
-        const checkRequiredFields = (entry) => {
-            const { author, published } = entry;
-            return !!author && !!author.name && !!published;
-        };
-
-        console.log(
-            "lol -> ",
-            webmentions
-                .filter((entry) => entry["wm-target"] === url)
-                .filter((entry) => allowedTypes.includes(entry["wm-property"]))
-                .filter(checkRequiredFields)
-                .sort(orderByDate)
-                .map(clean)
-        );
-
-        // run all of the above for each webmention that targets the current URL
-        return webmentions
-            .filter((entry) => entry["wm-target"] === url)
-            .filter((entry) => allowedTypes.includes(entry["wm-property"]))
-            .filter(checkRequiredFields)
-            .sort(orderByDate)
-            .map(clean);
-    });
-
+    // plugins
     config.addPlugin(pluginRss, {
         posthtmlRenderOptions: {
             closingSingleTag: "default", // opt-out of <img/>-style XHTML single tags
         },
+    });
+
+    // filters
+    config.addFilter("dateFilter", dateFilter);
+    config.addFilter("webmentionsForUrl", webmentionsFilter);
+    config.addFilter("isNan", (value) => {
+        return isNaN(value);
     });
 
     config.addWatchTarget("./src/blog/");
--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -1,47 +1,35 @@
 const fs = require("fs");
 const fetch = (...args) =>
     import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 const unionBy = require("lodash/unionBy");
-const site = require("./site.json");
+const domain = require("./site.json").domain;
 
 // Load .env variables with dotenv
 require("dotenv").config();
 
-// Configuration Parameters
-const CACHE_DIR = "_cache";
-const API_ORIGIN = "https://webmention.io/api/mentions.jf2";
+// Define Cache Location and API Endpoint
+const CACHE_FILE_PATH = "_cache/webmentions.json";
+const API = "https://webmention.io/api";
 const TOKEN = process.env.WEBMENTION_IO_TOKEN;
 
-async function fetchWebmentions(since) {
-    const { domain } = site;
-
-    if (!domain || domain === "myurl.com") {
-        // If we dont have a domain name, abort
+async function fetchWebmentions(since, perPage = 10000) {
+    // If we dont have a domain name or token, abort
+    if (!domain || !TOKEN) {
         console.warn(
-            "unable to fetch webmentions: no domain specified in metadata."
-        );
-        return false;
-    }
-    if (!TOKEN) {
-        // If we dont have a domain access token, abort
-        console.warn(
-            "unable to fetch webmentions: no access token specified in environment."
+            ">>> unable to fetch webmentions: missing domain or token"
         );
         return false;
     }
 
-    let url = `${API_ORIGIN}?domain=${domain}&token=${TOKEN}`;
-    if (since) {
-        url += `&per-page=100&&since=${since}`;
-    } else {
-        url += `&per-page=999`;
-    }
+    let url = `${API}/mentions.jf2?domain=${domain}&token=${TOKEN}&per-page=${perPage}`;
+    if (since) url += `&since=${since}`; // only fetch new mentions
 
     const response = await fetch(url);
     if (response.ok) {
         const feed = await response.json();
         console.log(
-            `${feed.children.length} webmentions fetched from ${API_ORIGIN}`
+            `>>> ${feed.children.length} new webmentions fetched from ${API}`
         );
         return feed;
     }
@@ -56,28 +44,27 @@ function mergeWebmentions(a, b) {
 
 // save combined webmentions in cache file
 function writeToCache(data) {
-    const filePath = `${CACHE_DIR}/webmentions.json`;
+    const dir = "_cache";
     const fileContent = JSON.stringify(data, null, 2);
-
     // create cache folder if it doesnt exist already
-    if (!fs.existsSync(CACHE_DIR)) {
-        fs.mkdirSync(CACHE_DIR);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
     }
     // write data to cache json file
-    fs.writeFile(filePath, fileContent, (err) => {
+    fs.writeFile(CACHE_FILE_PATH, fileContent, (err) => {
         if (err) throw err;
-        console.log(`webmentions cached to ${filePath}`);
+        console.log(`>>> webmentions cached to ${CACHE_FILE_PATH}`);
     });
 }
 
 // get cache contents from json file
 function readFromCache() {
-    const filePath = `${CACHE_DIR}/webmentions.json`;
-
-    if (fs.existsSync(filePath)) {
-        const cacheFile = fs.readFileSync(filePath);
+    if (fs.existsSync(CACHE_FILE_PATH)) {
+        const cacheFile = fs.readFileSync(CACHE_FILE_PATH);
         return JSON.parse(cacheFile);
     }
+
+    // no cache found.
     return {
         lastFetched: null,
         children: [],
@@ -85,13 +72,20 @@ function readFromCache() {
 }
 
 module.exports = async function () {
+    console.log(">>> Reading webmentions from cache...");
+
     const cache = readFromCache();
-    const { lastFetched } = cache;
+
+    if (cache.children.length) {
+        console.log(
+            `>>> ${cache.children.length} webmentions loaded from cache`
+        );
+    }
 
     // Only fetch new mentions in production
-    if (process.env.ELEVENTY_ENV === "production" || !lastFetched) {
-        const feed = await fetchWebmentions(lastFetched);
-
+    if (process.env.NODE_ENV === "production") {
+        console.log(">>> Checking for new webmentions...");
+        const feed = await fetchWebmentions(cache.lastFetched);
         if (feed) {
             const webmentions = {
                 lastFetched: new Date().toISOString(),
@@ -103,6 +97,5 @@ module.exports = async function () {
         }
     }
 
-    console.log(`${cache.children.length} webmentions loaded from cache`);
     return cache;
 };
